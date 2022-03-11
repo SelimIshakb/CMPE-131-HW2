c1caad93a19ca6be5f10880ba4f141354133883f
Selim Ishakbeyoglu <selim.ishakbeyoglu@sjsu.edu>
2022-03-10 23:45:25 -0800

    Added tracker.py

---
+++ tracker.py
@@ -0,0 +1,19 @@
+def func_counter(func):
+    counter = 0
+
+    def inner(x):
+        func(x)
+        inner.counter += 1
+
+    inner.counter = 0
+    return inner
+
+
+@func_counter
+def foo(y):
+    return y + 2 ** 3 - 34
+
+y = 1
+for _ in range(100):
+    foo(y)
+print(foo.counter)
